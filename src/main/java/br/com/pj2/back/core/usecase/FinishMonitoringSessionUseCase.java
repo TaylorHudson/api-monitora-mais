809447a35d4115fad97451fca15837b665f05c07
TaylorHudson <taylorhudsonsantos@gmail.com>
2025-05-19 22:03:34 -0300

    commenting unfinished classes

--- src/main/java/br/com/pj2/back/core/usecase/FinishMonitoringSessionUseCase.java
+++ src/main/java/br/com/pj2/back/core/usecase/FinishMonitoringSessionUseCase.java
@@ -1,20 +1,20 @@
-package br.com.pj2.back.core.usecase;
-
-import br.com.pj2.back.core.gateway.MonitoringScheduleGateway;
-import br.com.pj2.back.core.gateway.MonitoringSessionGateway;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class FinishMonitoringSessionUseCase {
-    private final MonitoringSessionGateway sessionGateway;
-    private final MonitoringScheduleGateway scheduleGateway;
-
-    public void execute(Long monitoringScheduleId, String description, String registration) {
-        var schedule = scheduleGateway.findByIdAndMonitorRegistration(monitoringScheduleId, registration);
-        var session = sessionGateway.findByMonitorAndIsStartedTrue(schedule.getMonitor());
-        session.finishSession(description);
-        sessionGateway.save(session);
-    }
-}
+//package br.com.pj2.back.core.usecase;
+//
+//import br.com.pj2.back.core.gateway.MonitoringScheduleGateway;
+//import br.com.pj2.back.core.gateway.MonitoringSessionGateway;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class FinishMonitoringSessionUseCase {
+//    private final MonitoringSessionGateway sessionGateway;
+//    private final MonitoringScheduleGateway scheduleGateway;
+//
+//    public void execute(Long monitoringScheduleId, String description, String registration) {
+//        var schedule = scheduleGateway.findByIdAndMonitorRegistration(monitoringScheduleId, registration);
+//        var session = sessionGateway.findByMonitorAndIsStartedTrue(schedule.getMonitor());
+//        session.finishSession(description);
+//        sessionGateway.save(session);
+//    }
+//}
--- src/main/java/br/com/pj2/back/core/usecase/StartMonitoringSessionUseCase.java
+++ src/main/java/br/com/pj2/back/core/usecase/StartMonitoringSessionUseCase.java
@@ -1,34 +1,34 @@
-package br.com.pj2.back.core.usecase;
-
-import br.com.pj2.back.core.domain.MonitoringSessionDomain;
-import br.com.pj2.back.core.exception.ResourceNotFoundException;
-import br.com.pj2.back.core.gateway.MonitoringScheduleGateway;
-import br.com.pj2.back.core.gateway.MonitoringSessionGateway;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class StartMonitoringSessionUseCase {
-    private final MonitoringSessionGateway sessionGateway;
-    private final MonitoringScheduleGateway scheduleGateway;
-
-    public void execute(Long monitoringScheduleId, String registration) {
-        var schedule = scheduleGateway.findByIdAndMonitorRegistration(monitoringScheduleId, registration);
-
-        checkIfSessionAlreadyStarted(schedule.getMonitor());
-
-        var session = new MonitoringSessionDomain(schedule.getMonitor(), schedule.getDiscipline());
-        session.startSession();
-        sessionGateway.save(session);
-    }
-
-    private void checkIfSessionAlreadyStarted(String registration) {
-        try {
-            var session = sessionGateway.findByMonitorAndIsStartedTrue(registration);
-            if (session != null) {
-                throw new IllegalStateException("A sessão de monitoria já foi iniciada.");
-            }
-        } catch (ResourceNotFoundException ignored) {}
-    }
-}
+//package br.com.pj2.back.core.usecase;
+//
+//import br.com.pj2.back.core.domain.MonitoringSessionDomain;
+//import br.com.pj2.back.core.exception.ResourceNotFoundException;
+//import br.com.pj2.back.core.gateway.MonitoringScheduleGateway;
+//import br.com.pj2.back.core.gateway.MonitoringSessionGateway;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class StartMonitoringSessionUseCase {
+//    private final MonitoringSessionGateway sessionGateway;
+//    private final MonitoringScheduleGateway scheduleGateway;
+//
+//    public void execute(Long monitoringScheduleId, String registration) {
+//        var schedule = scheduleGateway.findByIdAndMonitorRegistration(monitoringScheduleId, registration);
+//
+//        checkIfSessionAlreadyStarted(schedule.getMonitor());
+//
+//        var session = new MonitoringSessionDomain(schedule.getMonitor(), schedule.getDiscipline());
+//        session.startSession();
+//        sessionGateway.save(session);
+//    }
+//
+//    private void checkIfSessionAlreadyStarted(String registration) {
+//        try {
+//            var session = sessionGateway.findByMonitorAndIsStartedTrue(registration);
+//            if (session != null) {
+//                throw new IllegalStateException("A sessão de monitoria já foi iniciada.");
+//            }
+//        } catch (ResourceNotFoundException ignored) {}
+//    }
+//}
